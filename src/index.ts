70b078afa2442fe867949c17f1766b7fe94c563a
Hiro6634 <hiro.suyama@gamil.com>
2023-03-17 23:59:44 -0300

    resagado

--- src/index.ts
+++ src/index.ts
@@ -10,12 +10,12 @@ const hiroDID = 'did:quarkid:matic:EiAafYgtdgkBmeZRp86xtydU87PN8gdQ8H4DBu1u8bKfX
 const waciProtocol = new WACIProtocol({
     issuer:{
         issueCredentials: async (waciInvitationId: string, holderId: string) => {
-            return new WACICredentialOfferSucceded({
-                credentials: [{
-                    credential: {
+            return new WACIential: {
                         "@context": [
                             "https://www.w3.org/2018/credentials/v1",
-                            "https://www.w3.org/2018/credentials/examples/v1",
+                      CredentialOfferSucceded({
+                credentials: [{
+                    cred      "https://www.w3.org/2018/credentials/examples/v1",
                             "https://w3id.org/security/bbs/v1"
                         ],
                         id: "",
@@ -23,10 +23,12 @@ const waciProtocol = new WACIProtocol({
                             "VerifiableCredential",
                             "AlumniCredential"
                         ],
-                        issuer: "",
+                        issuer: "did:quarkid:matic:EiDs1liYifwFEg9l7rxrpR48MH-7Z-M2E32R1vEYThQWsQ",
                         issuanceDate: new Date(),
                         credentialSubject: {
-                            id: "",
+                            id: "did:quarkid:matic:EiAafYgtdgkBmeZRp86xtydU87PN8gdQ8H4DBu1u8bKfXA",
+                            giveName: "Jhon",
+                            familyName: "Does"
                         },
                     },
                     outputDescriptor: {
@@ -69,7 +71,81 @@ const waciProtocol = new WACIProtocol({
                         }
                     }
                 }],
+                issuer: {
+                    name: "Universidad Nacional",
+                    styles: {
+                        thumbnail: {
+                            uri: "https://dol.wa.com/logo.png",
+                            alt: "Universidad Nacional"
+                        },
+                        hero: {
+                            uri: "https://dol.wa.com/alumnos.png",
+                            alt: "Alumnos de la universidad"
+                        },
+                        background: {
+                            color: "#ff0000"
+                        },
+                        text: {
+                            color: "#d4d400"
+                        }
+                    }
+                },
+                options: {
+                    challenge: "508adef4-b8e0-4edf-a53d-a260371c1423",
+                    domain: "9rf25a28rs96"
+                },
             });
         }
     },
+    verifier: {
+        presentationDefinition: async (invitationId: string) => {
+            return {
+                frame: {
+                    "@context": [
+                        "https://www.w3.org/2018/credentials/v1",
+                        "https://www.w3.org/2018/credentials/examples/v1",
+                        "https://w3id.org/security/bbs/v1"
+                    ],
+                    "type": [
+                        "VerifiableCredential",
+                        "AlumniCredential"
+                    ],
+                    "credentialSubject": {
+                        "@explicit": true,
+                        "type": [
+                            "AlumniCredential"
+                        ],
+                        "givenName": {},
+                        "familyName": {}
+                    }
+                },
+                inputDescriptors: [
+                    {
+                        id: "Alumni Credential",
+                        name: "AlumniCredential",
+                        constraints: {
+                            fields: [
+                                {
+                                    path: [
+                                        "$.credentialSubject.givenName"
+                                    ],
+                                    filter: {
+                                        type: "string"
+                                    }
+                                },
+                                {
+                                    path: [
+                                        "$.credentialSubject.familyName"
+                                    ],
+                                    filter: {
+                                        type: "string"
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                ],
+            }
+        }
+    }
 });
